e2bda70ec59cea6fbca4071f26eebe4ad8048ddd
ahmet vurdem <vurdemahmet@gmail.com>
2021-05-13 21:01:27 +0300

    updated README.md file 2

--- src/test/java/com/optiim/utilities/BrowserUtils.java
+++ src/test/java/com/optiim/utilities/BrowserUtils.java
@@ -1,11 +1,8 @@
 package com.optiim.utilities;
 
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.io.File;
@@ -47,112 +44,5 @@ public class BrowserUtils {
             e.printStackTrace();
         }
     }
-
-    /**
-     * Waits for the provided element to be visible on the page
-     *
-     * @param element
-     * @param timeToWaitInSec
-     * @return
-     */
-    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeToWaitInSec);
-        return wait.until(ExpectedConditions.visibilityOf(element));
-    }
-
-    /**
-     * Waits for element matching the locator to be visible on the page
-     *
-     * @param locator
-     * @param timeout
-     * @return
-     */
-    public static WebElement waitForVisibility(By locator, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-    }
-
-    /**
-     * Waits for provided element to be clickable
-     *
-     * @param element
-     * @param timeout
-     * @return
-     */
-    public static WebElement waitForClickablility(WebElement element, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
-        return wait.until(ExpectedConditions.elementToBeClickable(element));
-    }
-
-    /**
-     * Waits for element matching the locator to be clickable
-     *
-     * @param locator
-     * @param timeout
-     * @return
-     */
-    public static WebElement waitForClickablility(By locator, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
-        return wait.until(ExpectedConditions.elementToBeClickable(locator));
-    }
-
-    /**
-     * Verifies whether the element matching the provided locator is displayed on page
-     *
-     * @param by
-     * @throws AssertionError if the element matching the provided locator is not found or not displayed
-     */
-    public static void verifyElementDisplayed(By by) {
-        try {
-            Assert.assertTrue(Driver.get().findElement(by).isDisplayed(), "Element not visible: " + by);
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            Assert.fail("Element not found: " + by);
-
-        }
-    }
-
-    /**
-     * Verifies whether the element matching the provided locator is NOT displayed on page
-     *
-     * @param by
-     * @throws AssertionError the element matching the provided locator is displayed
-     */
-    public static void verifyElementNotDisplayed(By by) {
-        try {
-            Assert.assertFalse(Driver.get().findElement(by).isDisplayed(), "Element should not be visible: " + by);
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-
-        }
-    }
-
-
-
-    /**
-     * Verifies whether the element is displayed on page
-     *
-     * @param element
-     * @throws AssertionError if the element is not found or not displayed
-     */
-    public static void verifyElementDisplayed(WebElement element) {
-        try {
-            Assert.assertTrue(element.isDisplayed(), "Element not visible: " + element);
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            Assert.fail("Element not found: " + element);
-
-        }
-    }
-
-    /**
-     * Scrolls down to an element using JavaScript
-     *
-     * @param element
-     */
-    public static void scrollToElement(WebElement element) {
-        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].scrollIntoView(true);", element);
-    }
-
 
 }
